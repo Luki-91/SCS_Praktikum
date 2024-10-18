443fd928a316965f04fdf66c66f7190916c5995c
Lukas Heiberger <lukas_heiberger@web.de>
2024-10-18 14:55:31 +0200

    commenting out everything not needed

--- volcano_plot.R
+++ volcano_plot.R
@@ -1,9 +1,10 @@
+library(ggrepel)
+setwd("C:/Users/lukas/Documents/CQ_Praktikum/full_DEG+CON")
 
-setwd("C:/Users/lukas/Documents/CQ_Praktikum/reclustered_DEG+CON")
 
-df<-read.csv2('cluster5.csv')
+df<-read.csv2('cluster12.csv')
 
-ggplot(data=df, aes(x=avg_log2FC, y=-log10(p_val_adj))) + geom_point()
+# ggplot(data=df, aes(x=avg_log2FC, y=-log10(p_val_adj))) + geom_point()
 
 # add a column of NAs
 df$diffexpressed <- "NO"
@@ -12,19 +13,17 @@ df$diffexpressed[df$avg_log2FC > 0.5 & df$p_val_adj < 0.05] <- "Vehicle"
 # if log2Foldchange < -0.6 and pvalue < 0.05, set as "DOWN"
 df$diffexpressed[df$avg_log2FC < -0.5 & df$p_val_adj < 0.05] <- "Wortmannin"
 
-p <- ggplot(data=df, aes(x=avg_log2FC, y=-log10(p_val_adj), col=diffexpressed)) + geom_point() + theme_minimal()
+#p <- ggplot(data=df, aes(x=avg_log2FC, y=-log10(p_val_adj), col=diffexpressed)) + geom_point() + theme_minimal()
 
 df$delabel <- NA
 df$delabel[df$diffexpressed != "NO"] <- df$X[df$diffexpressed != "NO"]
 
-ggplot(data=df, aes(x=avg_log2FC, y=-log10(p_val_adj), col=diffexpressed, label=delabel)) +
-  geom_point() +
-  theme_minimal() +
-  geom_text()
+#ggplot(data=df, aes(x=avg_log2FC, y=-log10(p_val_adj), col=diffexpressed, label=delabel)) +
+#  geom_point() +
+#  theme_minimal() +
+#  geom_text()
 
 # Finally, we can organize the labels nicely using the "ggrepel" package and the geom_text_repel() function
-# load library
-library(ggrepel)
 # plot adding up all layers we have seen so far
 ggplot(data=df, aes(x=avg_log2FC, y=-log10(p_val_adj), col=diffexpressed, label=delabel)) +
   geom_point() +
@@ -32,4 +31,4 @@ ggplot(data=df, aes(x=avg_log2FC, y=-log10(p_val_adj), col=diffexpressed, label=
   geom_text_repel() +
   scale_color_manual(values=c("black", "blue",  "red")) +
   geom_vline(xintercept=c(-0.5, 0.5), col="red") +
-  ggtitle('Cluster 5 Vehicel vs. Wortmannin')
+  ggtitle('Cluster 12 Vehicel vs. Wortmannin')
