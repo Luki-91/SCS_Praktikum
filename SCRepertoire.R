30a93979fc8f8d7d324d36ada9b54fa1f707b9d1
Lukas Heiberger <lukas_heiberger@web.de>
2024-10-22 16:46:11 +0200

    adding all steps to combine the TCR information into the SO

--- SCRepertoire.R
+++ SCRepertoire.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(scRepertoire)
 
 setwd("C:/Users/lukas/Documents/CQ_Praktikum")
-SO.har <- readRDS("SO.har.RDS")
+SO_TCR <- readRDS("SO_TCR.RDS")
 setwd("C:/Users/lukas/Documents/CQ_Praktikum/TCR/outs/vdj_t")
 tcr_v <- read.csv("filtered_contig_annotations.csv")
 
@@ -21,7 +21,7 @@ tcr_v$contig_id <- gsub("-1", "-3", tcr_v$contig_id)
 contig.list <- loadContigs(tcr_v, format = "10X")
 
 contig.list <- createHTOContigList(contig = tcr_v,
-                                   sc.data = SO.har,
+                                   sc.data = SO_TCR,
                                    group.by = "Replicates")
 
 head(contig_list[[1]])
@@ -31,6 +31,11 @@ combined.TCR <- combineTCR(contig.list,
                                        "BM_Wort_1", "BM_Wort_2",
                                        "Spleen_veh_1", "Spleen_veh_2",
                                        "Spleen_Wort_1", "Spleen_Wort_2"),
+                           removeNA = T,
+                           removeMulti = FALSE,
+                           filterMulti = F)
+
+all.TCR <- combineTCR(contig.list,
                            removeNA = T,
                            removeMulti = FALSE,
                            filterMulti = F)
@@ -84,3 +89,70 @@ clonalRarefaction(combined.TCR,
                   plot.type = 2,
                   hill.numbers = 1,
                   n.boots = 2)
+
+sub_combined <- clonalCluster(combined.TCR[[2]],
+                              chain = "TRA",
+                              sequence = "aa",
+                              threshold = 0.85,
+                              group.by = NULL)
+
+head(sub_combined[,c(1,2,13)])
+
+combined.TCR$BM_Veh_1$barcode <-
+  str_remove_all(combined.TCR$BM_Veh_1$barcode, "BM_Veh_1_")
+combined.TCR$BM_Veh_2$barcode <-
+  str_remove_all(combined.TCR$BM_Veh_2$barcode, "BM_Veh_2_")
+combined.TCR$BM_Wort_1$barcode <-
+  str_remove_all(combined.TCR$BM_Wort_1$barcode, "BM_Wort_1_")
+combined.TCR$BM_Wort_2$barcode <-
+  str_remove_all(combined.TCR$BM_Wort_2$barcode, "BM_Wort_2_")
+combined.TCR$Spleen_veh_1$barcode <-
+  str_remove_all(combined.TCR$Spleen_veh_1$barcode, "Spleen_veh_1")
+combined.TCR$Spleen_veh_2$barcode <-
+  str_remove_all(combined.TCR$Spleen_veh_2$barcode, "Spleen_veh_2")
+combined.TCR$Spleen_Wort_1$barcode <-
+  str_remove_all(combined.TCR$Spleen_Wort_1$barcode, "Spleen_Wort_1")
+combined.TCR$Spleen_Wort_2$barcode <-
+  str_remove_all(combined.TCR$Spleen_Wort_2$barcode, "Spleen_Wort_2")
+
+
+BM_Veh_1 <- subsetClones(combined.TCR, "sample", "BM_Veh_1")
+BM_Veh_2 <- subsetClones(combined.TCR, "sample", "BM_Veh_2")
+BM_Wort_1 <- subsetClones(combined.TCR, "sample", "BM_Wort_1")
+BM_Wort_2 <- subsetClones(combined.TCR, "sample", "BM_Veh_1")
+Spleen_veh_1 <- subsetClones(combined.TCR, "sample", "Spleen_veh_1")
+Spleen_veh_2 <- subsetClones(combined.TCR, "sample", "Spleen_veh_2")
+Spleen_Wort_1 <- subsetClones(combined.TCR, "sample", "Spleen_Wort_1")
+Spleen_Wort_2 <- subsetClones(combined.TCR, "sample", "BM_Veh_1")
+
+#subset_list <- c(BM_Veh_1,BM_Veh_2,BM_Wort_1,BM_Wort_2,
+#                 Spleen_veh_1,Spleen_veh_2,Spleen_Wort_1,Spleen_Wort_2)
+
+#### Doesn't work in a loop: the first argument has to be replaced everytime
+SO_TCR <- combineExpression(BM_Veh_1,
+                            SO_TCR,
+                            cloneCall="gene",
+                            proportion = TRUE)
+
+
+Disc_colors = c("#DC050C","#FB8072","#1965B0","#7BAFDE","#882E72","#B17BA6","#FF7F00","#FDB462","#E7298A","#E78AC3",
+
+                "#33A02C","#B2DF8A","#55A1B1","#8DD3C7","#A6761D","#E6AB02","#7570B3","#BEAED4","#666666","#999999",
+
+                "#aa8282","#d4b7b7","#8600bf","#ba5ce3","#808000","#aeae5c","#1e90ff","#00bfff","#56ff0d","#ffff00",
+
+
+
+                "#DC050C","#FB8072","#1965B0","#7BAFDE","#882E72","#B17BA6","#FF7F00","#FDB462","#E7298A","#E78AC3",
+
+                "#33A02C","#B2DF8A","#55A1B1","#8DD3C7","#A6761D","#E6AB02","#7570B3","#BEAED4","#666666","#999999",
+
+                "#aa8282","#d4b7b7","#8600bf","#ba5ce3","#808000","#aeae5c","#1e90ff","#00bfff","#56ff0d","#ffff00"
+
+)
+use_these_colors = c(Disc_colors[1:length(Idents(SO_TCR))])
+
+DimPlot(SO_TCR, group.by = "cloneSize",
+        split.by = "Condition",
+        label.size = 10, pt.size = .8,label = F, ncol = 2,
+        cols = use_these_colors)
